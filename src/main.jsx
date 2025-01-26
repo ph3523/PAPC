f72c19dbb6dcb5f6dc790ae7693e51da755a3253
EldFranklin <8wesleyfranklin@gmail.com>
2025-01-26 19:29:05 -0300

    Add footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import footer from './src/components/footer'; 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+    <Footer /> 
+  </StrictMode>
+);
