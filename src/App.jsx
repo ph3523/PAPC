e303b08876d8db2c47f35920676d371892413694
EldFranklin <8wesleyfranklin@gmail.com>
2025-01-27 11:02:37 -0300

    fix: ShowCadastro work

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Indicacao from './components/indicacao';
 import Home from './pages/Home';
 import Header from './components/header';
+import Cadastro from './components/cadastro'; // Importando o Cadastro
 
 function App() {
+  const [showCadastro, setShowCadastro] = useState(false); // Estado para controlar a visibilidade do modal
+
+  const handleOpenCadastro = () => setShowCadastro(true); // Função para abrir o modal
+  const handleCloseCadastro = () => setShowCadastro(false); // Função para fechar o modal
+
   return (
     <BrowserRouter>
-      <Header />
+      <Header onOpenCadastro={handleOpenCadastro} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/indicacao" element={<Indicacao />} />
-        <Route path="/cadastro" element={<cadastro />} /> {/* Adicione a rota para Cadastro */}
       </Routes>
+      
+      {/* Exibindo o modal Condicionalmente */}
+      {showCadastro && <Cadastro onClose={handleCloseCadastro} />}
     </BrowserRouter>
   );
 }
