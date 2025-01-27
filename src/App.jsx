d44f4f4f020da3b112acf1e17fb47d2fa7da65cb
Kelwin Gabriel <kelwimgabriel@gmail.com>
2025-01-26 21:05:27 -0300

    Tela login

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import Indicacao from './components/indicacao';
 import Home from './pages/Home';
+import Login from './components/login';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         {/* <Route path="/indicacao" element={<Indicacao />} /> */}
       </Routes>
     </BrowserRouter>
