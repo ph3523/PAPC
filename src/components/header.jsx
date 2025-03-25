1d87a49d0cb403ac400dd2cb38ef54f9258c6cfc
ph3523 <ph.barroso3523@gmail.com>
2025-03-25 11:44:28 -0300

    ::FEAT::Refatora a navegação do cadastro e ajusta o estilo do modal

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,7 @@
 import "./header.css";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { handleOpenCadastro } from "./cadastro";
+
 
 function Header() {
   return (
@@ -25,7 +25,7 @@ function Header() {
               <Nav.Link as={Link} to="/login">
                 Login
               </Nav.Link>
-              <Nav.Link onClick={handleOpenCadastro}>Cadastro</Nav.Link>
+              <Nav.Link as={Link} to="/cadastro">Cadastro</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
